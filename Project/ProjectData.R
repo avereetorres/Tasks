d5c31c08743ee840a847f9d6dc799029bf9c8e46
avereetorres <aat0008@mix.wvu.edu>
2023-02-23 21:00:27 -0500

    Averee Torres Project Data upload a

---
+++ Project/ProjectData.R
@@ -0,0 +1,6 @@
+Dog <- read.csv("/Users/AVEREE/Desktop/Evolution/Project/Dog Data.csv", header=TRUE, stringsAsFactors=FALSE)
+Dog
+options(max.print=999999)
+
+#Hypothesis
+#Will variations in allele frequencies effect skull and ear morphology within different dog breeds?
